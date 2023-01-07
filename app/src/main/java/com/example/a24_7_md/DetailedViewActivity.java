445ff26c1c05d97f1db84e0ce540ee0c1b4f0dd9
Shihao Liu <lsh20011018@gmail.com>
2023-01-07 14:42:03 -0700

    DetailedView and ViewDisease activities initialized

---
+++ app/src/main/java/com/example/a24_7_md/DetailedViewActivity.java
@@ -0,0 +1,14 @@
+package com.example.a24_7_md;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailedViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_view);
+    }
+}
